c0e03a99673f93cf3371bd3fa46c67f9b4298429
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-23 19:42:00 +0100

    i

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,23 +6,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int ui;
-int length, power_of_two;
-if (!b)
-return (0);
-ui = 0;
-for (length = 0; b[length] != '\0'; length++)
-;
-for (length--, power_of_two = 1; length >= 0; length--, power_of_two *= 2)
-{
-if (b[length] != '0' && b[length] != '1')
-{
-return (0);
-}
-if (b[length] & 1)
-{
-ui += power_of_two;
-}
-}
-return (ui);
+    unsigned int ui;
+    int length, power_of_two;
+    if (!b)
+        return (0);
+    ui = 0;
+    for (length = 0; b[length] != '\0'; length++)
+        ;
+    for (length--, power_of_two = 1; length >= 0; length--, power_of_two *= 2)
+    {
+        if (b[length] != '0' && b[length] != '1')
+        {
+            return (0);
+        }
+        if (b[length] & 1)
+        {
+            ui += power_of_two;
+        }
+    }
+    return (ui);
 }
