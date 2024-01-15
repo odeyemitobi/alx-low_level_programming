79660d3f24a9378ac283b458be7ae0eb435039c4
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-15 14:01:05 +0100

    Still fixing error for task 2

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -6,5 +6,8 @@ gcc -Wall -fPIC -c *.c
 # Create the dynamic library liball.so from the object files
 gcc -shared -o liball.so *.o
 
+# Display the symbols in the dynamic library
+nm -D --defined-only liball.so
+
 # Clean up: remove the object files
 # rm *.o
