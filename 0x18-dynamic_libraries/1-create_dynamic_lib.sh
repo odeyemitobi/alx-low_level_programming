3a85dd6f90e6d9f19b235a2d549b875383c616d8
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2024-01-15 13:52:11 +0100

    Task 2 testing

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Compile all .c files in the current directory into object files
+gcc -Wall -fPIC -c *.c
+
+# Create the dynamic library liball.so from the object files
+gcc -shared -o liball.so *.o
--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -9,7 +9,7 @@
  */
 void custom_putchar(char c)
 {
-	write(1, &c, 1); // File descriptor 1 corresponds to standard output
+	write(1, &c, 1);
 }
 
 /**
@@ -19,118 +19,96 @@ void custom_putchar(char c)
  */
 int main()
 {
-	// Example usage of functions from libdynamic.so
-
-	// _putchar
 	_putchar('H');
 	_putchar('i');
 	_putchar('\n');
 
-	// _islower
-	custom_putchar('0' + _islower('a')); // Should print '1'
-	custom_putchar('0' + _islower('A')); // Should print '0'
+	custom_putchar('0' + _islower('a'));
+	custom_putchar('0' + _islower('A'));
 	custom_putchar('\n');
 
-	// _isalpha
-	custom_putchar('0' + _isalpha('a')); // Should print '1'
-	custom_putchar('0' + _isalpha('1')); // Should print '0'
+	custom_putchar('0' + _isalpha('a'));
+	custom_putchar('0' + _isalpha('1'));
 	custom_putchar('\n');
 
-	// _abs
 	int absResult = _abs(-5);
-	custom_putchar('0' + (absResult / 10)); // Should print '5'
-	custom_putchar('0' + (absResult % 10)); // Should print '5'
+	custom_putchar('0' + (absResult / 10));
+	custom_putchar('0' + (absResult % 10));
 	custom_putchar('\n');
 
-	// _isupper
-	custom_putchar('0' + _isupper('A')); // Should print '1'
-	custom_putchar('0' + _isupper('a')); // Should print '0'
+	custom_putchar('0' + _isupper('A'));
+	custom_putchar('0' + _isupper('a'));
 	custom_putchar('\n');
 
-	// _isdigit
-	custom_putchar('0' + _isdigit('5')); // Should print '1'
-	custom_putchar('0' + _isdigit('a')); // Should print '0'
+	custom_putchar('0' + _isdigit('5'));
+	custom_putchar('0' + _isdigit('a'));
 	custom_putchar('\n');
 
-	// _strlen
 	char str[] = "Hello, World!";
 	int length = _strlen(str);
-	custom_putchar('0' + (length / 10)); // Should print '1'
-	custom_putchar('0' + (length % 10)); // Should print '3'
+	custom_putchar('0' + (length / 10));
+	custom_putchar('0' + (length % 10));
 	custom_putchar('\n');
 
-	// _puts
 	_puts("Hello, Dynamic Library");
 
-	// _strcpy
 	char dest[20];
 	_strcpy(dest, "Copy this!");
 	_puts(dest);
 
-	// _atoi
 	int atoiResult = _atoi("123");
-	custom_putchar('0' + (atoiResult / 100));		// Should print '1'
-	custom_putchar('0' + ((atoiResult / 10) % 10)); // Should print '2'
-	custom_putchar('0' + (atoiResult % 10));		// Should print '3'
+	custom_putchar('0' + (atoiResult / 100));
+	custom_putchar('0' + ((atoiResult / 10) % 10));
+	custom_putchar('0' + (atoiResult % 10));
 	custom_putchar('\n');
 
-	// _strcat
 	char destCat[30] = "Hello, ";
 	_strcat(destCat, "World!");
 	_puts(destCat);
 
-	// _strncat
 	char destNcat[30] = "Hello, ";
 	_strncat(destNcat, "World!", 3);
-	_puts(destNcat); // Should print "Hello, Wor"
+	_puts(destNcat);
 
-	// _strncpy
 	char destNcpy[30];
 	_strncpy(destNcpy, "Copy this!", 7);
-	_puts(destNcpy); // Should print "Copy th"
+	_puts(destNcpy);
 
-	// _strcmp
 	int strcmpResult = _strcmp("abc", "abcd");
-	custom_putchar('0' + (strcmpResult / 100));				 // Should print '-'
-	custom_putchar('0' + (((strcmpResult * -1) / 10) % 10)); // Should print '3'
-	custom_putchar('0' + ((strcmpResult * -1) % 10));		 // Should print '2'
+	custom_putchar('0' + (strcmpResult / 100));
+	custom_putchar('0' + (((strcmpResult * -1) / 10) % 10));
+	custom_putchar('0' + ((strcmpResult * -1) % 10));
 	custom_putchar('\n');
 
-	// _memset
 	char memSetArr[10];
 	_memset(memSetArr, '*', 5);
-	_puts(memSetArr); // Should print "*****"
+	_puts(memSetArr);
 
-	// _memcpy
 	char srcMemCpy[5] = "12345";
 	char destMemCpy[5];
 	_memcpy(destMemCpy, srcMemCpy, 5);
-	_puts(destMemCpy); // Should print "12345"
+	_puts(destMemCpy);
 
-	// _strchr
 	char haystack[] = "Find me!";
 	char *foundChar = _strchr(haystack, 'm');
-	_puts(foundChar); // Should print "me!"
+	_puts(foundChar);
 
-	// _strspn
 	char spanStr[] = "12345";
 	char acceptStr[] = "123";
 	int strspnResult = _strspn(spanStr, acceptStr);
-	custom_putchar('0' + (strspnResult / 10)); // Should print '3'
-	custom_putchar('0' + (strspnResult % 10)); // Should print '0'
+	custom_putchar('0' + (strspnResult / 10));
+	custom_putchar('0' + (strspnResult % 10));
 	custom_putchar('\n');
 
-	// _strpbrk
 	char pbrkStr[] = "abcde";
 	char acceptPbrk[] = "xyzb";
 	char *foundPbrk = _strpbrk(pbrkStr, acceptPbrk);
-	_puts(foundPbrk); // Should print "bc"
+	_puts(foundPbrk);
 
-	// _strstr
 	char haystackStr[] = "Hello, World!";
 	char needleStr[] = "World";
 	char *foundStr = _strstr(haystackStr, needleStr);
-	_puts(foundStr); // Should print "World!"
+	_puts(foundStr);
 
 	return 0;
 }
